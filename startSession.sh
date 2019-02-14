9363920c1d558c5a921947536925af1cab57d6d5
lrast <luke.rast@gmail.com>
2019-02-14 14:27:42 -0500

    remove u name

--- startSession.sh
+++ startSession.sh
@@ -3,6 +3,6 @@ screen -d -m jupyter notebook --port=$1 --browser='none'
 echo $1:127.0.0.1:$1
 
 # port forward back to the login machine. Also holds the connection open
-ssh -N -v -R $1:127.0.0.1:$1 lr93@$2
+ssh -N -v -R $1:127.0.0.1:$1 myUser@$2
 
 
